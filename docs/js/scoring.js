d1bd7b06ad6bd34f173e3fa6d3b74d1f5c086bc0
Tatsuya Okazaki <180442029@ccmailg.meijo-u.ac.jp>
2021-01-22 04:39:03 +0900

    全てを変更した。スタイルに初め、レスポンスにも全て変更をした

--- docs/js/scoring.js
+++ docs/js/scoring.js
@@ -1,15 +1,10 @@
 function onScoringButton(){
-    console.log("ボタン押されたで");
-    myURL = location.href;
-    let year = myURL.split("#")[1];
+    let municipality = getParam("municipality");
+    let year = getParam("year");
     let C_A = [[]];
     let mistake_Answer = [];
-    if(year=="2019"){
-        C_A = C_2019_A();
-    }else if(year=="2018"){
-        C_A = C_2018_A();
-    }else if(year=="2017"){
-        C_A = C_2017_A();
+    if(year || municipality){
+        C_A = getCA(year,municipality)
     }else{
         console.log("エラー発生");
         return
@@ -23,8 +18,6 @@ function onScoringButton(){
             mistake_Answer.push((i+1)+"");
         }
     }
-    console.log(score);
-    console.log(mistake_Answer);
     for(i = 0 ; i < mistake_Answer.length ; i++){
         document.getElementById("q"+mistake_Answer[i]).style = "color: red; font-weight: 900;";
     }
@@ -37,36 +30,65 @@ function onScoringButton(){
     document.getElementById("score_unit").innerHTML = "点";
 }
 
-function C_2019_A() {
-    // 正解の回答
-    const Correct_Answer = [8,3,2,1,7,3,4,2,5,5,
-                            9,5,2,5,6,4,3,4,2,4,
-                            0,3,8,6,1,6,9,8,7,1];
-    // 配点
-    const Allocation = [4,4,4,4,4,4,4,4,4,4,
-                        3,3,3,3,3,3,3,3,3,3,
-                        3,3,3,3,3,3,3,3,3,3,];
-    return [Correct_Answer,Allocation];
+function getCA(year,municipality){
+    const CA = {
+        aichi:{
+            2019:[
+                [
+                    8,3,2,1,7,3,4,2,5,5,
+                    9,5,2,5,6,4,3,4,2,4,
+                    0,3,8,6,1,6,9,8,7,1
+                ],
+                [
+                    4,4,4,4,4,4,4,4,4,4,
+                    3,3,3,3,3,3,3,3,3,3,
+                    3,3,3,3,3,3,3,3,3,3
+                ]
+            ],
+            2018:[
+                [
+                    5,4,5,6,1,1,8,1,2,3,
+                    4,7,2,3,8,4,7,9,7,6,
+                    4,7,9,3,1,8,6,0,2,0
+                ],
+                [
+                    4,4,4,4,4,4,4,4,4,4,
+                    3,3,3,3,3,3,3,3,3,3,
+                    3,3,3,3,3,3,3,3,3,3
+                ]
+                
+            ],
+            2017:[
+                [
+                    6,3,7,4,4,6,1,7,1,8,
+                    0,5,1,5,5,3,5,7,6,1,
+                    3,6,2,8,2,7,3,2,8,4
+                ],
+                [
+                    4,4,4,4,4,4,4,4,4,4,
+                    3,3,3,3,3,3,3,3,3,3,
+                    3,3,3,3,3,3,3,3,3,3
+                ]
+            ]
+        }
+    }   
+    return CA[municipality][year]
 }
-function C_2018_A() {
-    // 正解の回答
-    const Correct_Answer = [5,4,5,6,1,1,8,1,2,3,
-                            4,7,2,3,8,4,7,9,7,6,
-                            4,7,9,3,1,8,6,0,2,0];
-    // 配点
-    const Allocation = [4,4,4,4,4,4,4,4,4,4,
-                        3,3,3,3,3,3,3,3,3,3,
-                        3,3,3,3,3,3,3,3,3,3,];
-    return [Correct_Answer,Allocation];
+
+
+// クエリのパラメタを取得する関数
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
-function C_2017_A() {
-    // 正解の回答
-    const Correct_Answer = [6,3,7,4,4,6,1,7,1,8,
-                            0,5,1,5,5,3,5,7,6,1,
-                            3,6,2,8,2,7,3,2,8,4];
-    // 配点
-    const Allocation = [4,4,4,4,4,4,4,4,4,4,
-                        3,3,3,3,3,3,3,3,3,3,
-                        3,3,3,3,3,3,3,3,3,3,];
-    return [Correct_Answer,Allocation];
-}
+// ロード時に実行するスクリプト
+document.addEventListener("DOMContentLoaded", function() {
+    const preaf = {"aichi":"愛知県","nagoya":"名古屋"};
+    document.getElementById("municipality").innerHTML = preaf[getParam("municipality")];
+    document.getElementById("year").innerHTML = getParam("year") + "年";
+ });
